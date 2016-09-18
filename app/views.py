036209b2ec83cb6907c7d45444c69e93ea24152d
Ken McLoud <ken.mcloud@gmail.com>
2016-09-17 20:20:25 -0400

    inserted logs into db, installed bootstrap

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
 from app import app
 from flask import request
+from flask import render_template
 from app import db, models
 from datetime import datetime
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html',title='Home Page')
 
 @app.route('/submit_data', methods=['GET','POST'])
 def submit_data():
@@ -13,10 +14,26 @@ def submit_data():
         return 'Submit data here'
     if request.method == 'POST':
         form_data = request.form
-        d = models.data_log(source=form_data['source'],
-                            datatype=form_data['datatype'],
-                            time=datetime.now(),
-                            payload=form_data['payload'])
-        db.session.add(d)
-        db.session.commit()
+
+        dt_obj = datetime.strptime(form_data['timestamp'],'%Y-%m-%d %H:%M:%S')
+        
+        #check if entry exists, doesn't work
+        prev=models.data_log.query.filter_by(
+            time=dt_obj).filter_by(source=form_data['source']).all()
+
+        if len(prev)==0:
+        
+            d = models.data_log(source=form_data['source'],
+                                datatype=form_data['datatype'],
+                                time=dt_obj,
+                                payload=form_data['payload'])
+            db.session.add(d)
+            db.session.commit()
+            print('data entered')
+
+        else:
+            print('duplicate entry')
+            
         return '200 OK'
+
+
--- test_post.py
+++ test_post.py
@@ -1,4 +1,4 @@
 #!flask/bin/python
 import requests
-r=requests.post('http://localhost:5000/submit_data', data={'source':'dev_machine','datatype':'tool life','payload':'t1 50,100 t2 20,200'})
+r=requests.post('http://localhost:5000/submit_data', data={'timestamp':'2016-02-01 01:01:01','source':'dev_machine','datatype':'tool life','payload':'t1 50,100 t2 20,200'})
 
